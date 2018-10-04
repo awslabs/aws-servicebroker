986453ba0cd9983e9dc5ff85145018bcee3fc722
Jay McConnell <jmmccon@amazon.com>
2018-10-03 22:36:30 -0700

    fix typo

--- packaging/openshift/deploy.sh
+++ packaging/openshift/deploy.sh
@@ -2,6 +2,6 @@
 
 ACCESSKEYID=$1
 SECRETKEY=$2
-oc project aws-sb
+oc new-project aws-sb
 CA=`oc get secret -n kube-service-catalog -o go-template='{{ range .items }}{{ if eq .type "kubernetes.io/service-account-token" }}{{ index .data "service-ca.crt" }}{{end}}{{"\n"}}{{end}}' | tail -n 1`
 oc process -f aws-servicebroker.yaml --param-file=parameters.env -p BROKER_CA_CERT=$CA -p ACCESSKEYID=${ACCESSKEYID} -p SECRETKEY=${SECRETKEY} | oc apply -f -
