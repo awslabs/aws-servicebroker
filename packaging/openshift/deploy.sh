bbc68704fe9c232bc6db897f1d4854469bae81ad
Jay McConnell <jmmccon@amazon.com>
2018-10-10 08:09:15 -0700

    some versions of oc add blank lines to the end of output

--- packaging/openshift/deploy.sh
+++ packaging/openshift/deploy.sh
@@ -3,5 +3,5 @@
 ACCESSKEYID=$1
 SECRETKEY=$2
 oc new-project aws-sb
-CA=`oc get secret -n kube-service-catalog -o go-template='{{ range .items }}{{ if eq .type "kubernetes.io/service-account-token" }}{{ index .data "service-ca.crt" }}{{end}}{{"\n"}}{{end}}' | tail -n 1`
+CA=`oc get secret -n kube-service-catalog -o go-template='{{ range .items }}{{ if eq .type "kubernetes.io/service-account-token" }}{{ index .data "service-ca.crt" }}{{end}}{{"\n"}}{{end}}' | grep -v '^$' | tail -n 1`
 oc process -f aws-servicebroker.yaml --param-file=parameters.env -p BROKER_CA_CERT=$CA -p ACCESSKEYID=${ACCESSKEYID} -p SECRETKEY=${SECRETKEY} | oc apply -f -
