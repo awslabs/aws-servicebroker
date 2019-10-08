acd1679b7dd9a139ddaf54d76cfff2b43073868b
Kin-Wai Koo <glug71@gmail.com>
2019-10-08 17:59:24 +0800

    Switch to aws-sb before deploying the AWS service broker

--- packaging/openshift/deploy.sh
+++ packaging/openshift/deploy.sh
@@ -2,8 +2,10 @@
 
 ACCESSKEYID=$(echo -n $1 | base64)
 SECRETKEY=$(echo -n $2 | base64)
-oc new-project aws-sb
+
 # On OpenShift 4.x the project name has changed to "openshift-service-catalog-apiserver"
 oc project kube-service-catalog || oc project openshift-service-catalog-apiserver || echo "Warning: Cannot find project ..."
-CA=`oc get secret                         -o go-template='{{ range .items }}{{ if eq .type "kubernetes.io/service-account-token" }}{{ index .data "service-ca.crt" }}{{end}}{{"\n"}}{{end}}' | grep -v '^$' | tail -n 1`
+CA=`oc get secret -o go-template='{{ range .items }}{{ if eq .type "kubernetes.io/service-account-token" }}{{ index .data "service-ca.crt" }}{{end}}{{"\n"}}{{end}}' | grep -v '^$' | tail -n 1`
+
+oc new-project aws-sb || oc project aws-sb
 oc process -f aws-servicebroker.yaml --param-file=parameters.env -p BROKER_CA_CERT=$CA -p ACCESSKEYID=${ACCESSKEYID} -p SECRETKEY=${SECRETKEY} | oc apply -f -
